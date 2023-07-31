f1f20d009c6cfb696ec45f16e5bfc2ee91e382d0
Jagerziel <fromnytosg@gmail.com>
2023-07-31 15:49:38 -0400

    added Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Typography , Box, useTheme } from '@mui/material'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+const Header = ( { title , subTitle }) => {
+    const theme = useTheme()
+
+    return (
+        <Box>
+           <Typography 
+                variant="h2" 
+                color={theme.palette.secondary[100]}
+                fontWeight="bold"
+                sx={{marginBottom: "5px"}}
+            >
+                {title}
+            </Typography>
+            <Typography 
+                variant="h5" 
+                color={theme.palette.secondary[300]}
+            >
+                {subTitle}
+            </Typography>
+        </Box>
+    )
 }
 
 export default Header
